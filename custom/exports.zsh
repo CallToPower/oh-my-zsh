1f143b084178b655cc4114087858cbedd417d68c
Denis Meyer <denis.meyer@mindsmash.com>
2015-12-01 15:51:07 +0100

    Node installation

--- custom/exports.zsh
+++ custom/exports.zsh
@@ -1,8 +1,13 @@
 export EDITOR="open -a /Applications/TextMate.app"
-export NODE_PATH="/usr/local/lib/node"
 export PATH="/usr/local/bin/:/opt/play-1.2.5.3/:$PATH"
 export PATH="/Library/PostgreSQL/9.5/bin/:$PATH"
 # export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin"
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=128M -Djava.security.egd=file:/dev/./urandom"
 export COYO_DB_HOST="localhost"
 export COYO_DB_PORT=5432
+
+# for the node/npm installation described at http://www.johnpapa.net/how-to-use-npm-global-without-sudo-on-osx/
+export NPM_PACKAGES="/Users/denmeyer/.npm-packages"
+export NODE_PATH="/usr/local/bin/node"
+export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+export PATH="$NPM_PACKAGES/bin:$PATH"
